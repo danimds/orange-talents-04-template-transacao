0af1b286d7cabf70e9d12023a869b02ec1cad9b2
Danielle <danimartinsds17@gmail.com>
2021-05-28 10:03:47 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/dani/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.dani.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
